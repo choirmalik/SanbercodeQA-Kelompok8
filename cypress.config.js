350ecb17e076cf2f201bebe9a60bf459e99eafa5
Yosephine Herlina Megawati Siburian <103814956+Yosephine25@users.noreply.github.com>
2023-08-02 14:35:20 +0700

    Add files via upload
    Sign In

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,6 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  reporter: 'mochawesome',
-  reporterOptions: {
-    overwrite: false,
-    html: false,
-    json: true,
-  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
