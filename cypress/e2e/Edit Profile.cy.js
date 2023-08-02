a2f0a2c8295e1d757724643150c3d2bd4902c6ba
Juan Daniel F.P <140182929+juand21695@users.noreply.github.com>
2023-08-02 21:37:39 +0700

    Edit Profile dan Tambahin Video Run

--- cypress/e2e/Edit Profile.cy.js
+++ cypress/e2e/Edit Profile.cy.js
@@ -1,10 +1,14 @@
 describe('Edit Profile', () => {
   it('Edit Profile', () => {
-    //url
     cy.visit('https://magento.softwaretestingboard.com/')
     cy.visit('https://magento.softwaretestingboard.com/customer/account/login/referer/aHR0cHM6Ly9tYWdlbnRvLnNvZnR3YXJldGVzdGluZ2JvYXJkLmNvbS8%2C/')    
-    cy.get('#email').type('juand21695@gmail.com') 
-    cy.get('#pass').type('juan1234!')
+    cy.get('#email').type('febrison009@gmail.com') 
+    cy.get('#pass').type('test1234!')
     cy.get('#send2').click({force: true})
+    cy.get(":nth-child(2) > .customer-welcome > .customer-menu > .header > :nth-child(1) > a"). click({force : true})
+    cy.get('.block-dashboard-info > .block-content > .box > .box-actions > .edit > span').click({force : true})
+    cy.get('#firstname').type('Febri').clear()
+    cy.get('#firstname').type('testing123')
+    cy.get('#form-validate > .actions-toolbar > div.primary > .action > span').click({force : true})
   })
 })  
--- cypress/e2e/SignUp.cy.js
+++ cypress/e2e/SignUp.cy.js
@@ -2,15 +2,14 @@ describe('Sign UP', () => {
   it('Sign Up', () => {
     //url
     cy.visit('https://magento.softwaretestingboard.com/')
-    cy.visit('https://magento.softwaretestingboard.com/customer/account/create/')    
-    cy.get('#firstname').type('Juan')
-    cy.get('#lastname').type('Daniel')
-    cy.get('#email_address').type('juand21695@gmail.com') 
-    cy.get('#password').type('juan1234!')
-    cy.get('#password-confirmation').type('juan1234!')
+    cy.get('.panel > .header > :nth-child(3) > a').click({force : true})   
+    cy.get('#firstname').type('Febri')
+    cy.get('#lastname').type('Son')
+    cy.get('#email_address').type('febrison009@gmail.com') 
+    cy.get('#password').type('test1234!')
+    cy.get('#password-confirmation').type('test1234!')
     cy.get('.submit').click()
     cy.get('.action.switch').click()
-    
     cy.get('select').select('My Account')
     
   })
