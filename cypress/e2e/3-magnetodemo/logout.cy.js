4829b59726e47e1a3120e0ade9899d1f4ef47c06
m.choirudin <m.choirudin@elnusa.co.id>
2023-08-02 15:31:53 +0700

    Sign Out Test

--- cypress/e2e/3-magnetodemo/logout.cy.js
+++ cypress/e2e/3-magnetodemo/logout.cy.js
@@ -1,10 +1,16 @@
-describe('template spec', () => {
-    it('passes', () => {
+describe('Logout Test', () => {
+    it('Verifikasi dapat logout setelah berhasil login', () => {
+      // Login
       cy.visit('https://magento.softwaretestingboard.com/customer/account/login/referer/aHR0cHM6Ly9tYWdlbnRvLnNvZnR3YXJldGVzdGluZ2JvYXJkLmNvbS8%2C/')
       cy.get('#email').type("yosephineherlina20040205@gmail.com")
       cy.get('.login-container > .block-customer-login > .block-content > #login-form > .fieldset > .password > .control > #pass').type("@Yosephine2004")
       cy.get('.login-container > .block-customer-login > .block-content > #login-form > .fieldset > .actions-toolbar > div.primary > #send2 > span').click()
-      Navigation.visit
-      cy.get('body')
+      
+      // Logout
+      cy.get('[data-action="customer-menu-toggle"]').click({multiple : true, force: true})
+      cy.get(':nth-child(2) > .customer-welcome > .customer-menu > .header > .authorization-link > a').click({multiple : true, force: true})
+      
+      cy.get('.panel > .header > .authorization-link > a').should('be.visible')
+      cy.get('.panel > .header > :nth-child(3) > a').should('be.visible')
     })
   })
